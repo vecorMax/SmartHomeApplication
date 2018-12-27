3b8ed79407358aedecc7b3ac98d7cf4fc8777192
Илья Селетков <iseletkov@gmail.com>
2018-12-27 11:23:56 +0500

    1. Использованы библиотеки androidx. 2. Меню приложения написано в соответствии с официальной документацией. 3. Добавлена икнка для пункта меню search. 4. Панель инструментов написана в соответствии с официальной документацией.

--- app/src/main/java/com/smarthome/Activities/CActivityMain.java
+++ app/src/main/java/com/smarthome/Activities/CActivityMain.java
@@ -1,60 +1,37 @@
 package com.smarthome.Activities;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import android.arch.lifecycle.ViewModelProvider;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
-import com.smarthome.NATS;
-import com.smarthome.Notifications.Notify;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.smarthome.R;
 import com.smarthome.Services.ServiceNotify;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import io.nats.client.Connection;
-import io.nats.client.Message;
-import io.nats.client.MessageHandler;
-import io.nats.client.Nats;
-import io.nats.client.Options;
-import io.nats.client.Statistics;
-import io.nats.client.Subscription;
-import io.nats.client.SyncSubscription;
 
-public class MainActivity extends AppCompatActivity {
+public class CActivityMain extends AppCompatActivity {
 
     public Switch switch_temp;
     final String LOG_TAG  = "status";
     public static final String APP_PREFERENCES_SWITCH_TEMPERATURE = "settings";
     public static final String APP_PREFERENCES_COUNTER_SWITCH_TEMPERATURE = "switch_temp";
     private SharedPreferences mSettings;
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         Log.d(LOG_TAG,"MainActivity: onCreate()");
+         Log.d(LOG_TAG,"CActivityMain: onCreate()");
 
         initToolbar();
 
@@ -85,22 +62,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initToolbar() {
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Welcome");
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                return false;
-            }
-        });
-
-        toolbar.inflateMenu(R.menu.menu);
+        Toolbar toolbar                     = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar()!=null)
+        {
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            getSupportActionBar().setTitle("Welcome");
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(LOG_TAG,"MainActivity: onResume()");
+        Log.d(LOG_TAG,"CActivityMain: onResume()");
         if (mSettings.contains(APP_PREFERENCES_COUNTER_SWITCH_TEMPERATURE)) {
             // Получаем данные из настроек
             switch_temp.setChecked(mSettings.getBoolean(APP_PREFERENCES_COUNTER_SWITCH_TEMPERATURE, false));
@@ -110,47 +84,80 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(LOG_TAG,"MainActivity: onPause()");
+        Log.d(LOG_TAG,"CActivityMain: onPause()");
         // Запоминаем данные
         SharedPreferences.Editor editor = mSettings.edit();
-        editor.putBoolean(APP_PREFERENCES_COUNTER_SWITCH_TEMPERATURE, switch_temp.isChecked());
+//        editor.putBoolean(APP_PREFERENCES_COUNTER_SWITCH_TEMPERATURE, switch_temp.isChecked());
         editor.apply();
     }
 
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        switch_temp.setChecked(savedInstanceState.getBoolean("switch"));
-        Log.d(LOG_TAG, "MainActivity: onRestoreInstanceState()");
+//        switch_temp.setChecked(savedInstanceState.getBoolean("switch"));
+        Log.d(LOG_TAG, "CActivityMain: onRestoreInstanceState()");
     }
 
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putBoolean("switch",switch_temp.isChecked());
-        Log.d(LOG_TAG, "MainActivity: onSaveInstanceState()");
+//        outState.putBoolean("switch",switch_temp.isChecked());
+        Log.d(LOG_TAG, "CActivityMain: onSaveInstanceState()");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(LOG_TAG,"MainActivity: onStart()");
+        Log.d(LOG_TAG,"CActivityMain: onStart()");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(LOG_TAG, "MainActivity: onRestart()");
+        Log.d(LOG_TAG, "CActivityMain: onRestart()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(LOG_TAG,"MainActivity: onStop()");
+        Log.d(LOG_TAG,"CActivityMain: onStop()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(LOG_TAG,"MainActivity: onDestroy()");
+        Log.d(LOG_TAG,"CActivityMain: onDestroy()");
     }
 
+    /****************************************************************************************************
+     * Действия при создании меню.                                                                      *
+     * @param menu - заготовка для меню.                                                                *
+     * @return                                                                                          *
+     ***************************************************************************************************/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater               = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    /****************************************************************************************************
+     * Обработка нажатий на элемент меню.                                                               *
+     * @param item - элемент меню, на который нажал пользователь.                                       *
+     * @return                                                                                          *
+     ***************************************************************************************************/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle item selection
+        switch (item.getItemId())
+        {
+            case R.id.search:
+
+                return true;
+//            case R.id.help:
+//                showHelp();
+//                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
